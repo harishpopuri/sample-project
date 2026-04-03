037297cbd3d2c83c42473d25482ec6a7cc0f27dc
harishpopuri <40349129+harishpopuri@users.noreply.github.com>
2026-04-02 20:54:15 -0500

    A3 Function to check if a number is prime
    Function to check if a number is prime

--- sample.py
+++ sample.py
@@ -21,4 +21,15 @@ try:
 
 except ZeroDivisionError:
     print("Error: Division by zero is not allowed.")
+## `A3 Function to check if a number is prime`
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+number = 17
+if is_prime(number):
+    print(f"{number} is a prime number.")   
 
